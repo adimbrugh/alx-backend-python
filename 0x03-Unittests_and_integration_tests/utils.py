a0b66a65e45e54596d3965007f78fd0aa7f0ba07
adimbrugh <haisamismail71@gmail.com>
2025-07-15 16:05:03 +0400

    More patching.

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,5 +1,28 @@
+#!/usr/bin/env python3
+"""Utility functions for unit testing."""
+import requests
 
 
-nested_map={"a": 1}, path=("a",)
-nested_map={"a": {"b": 2}}, path=("a",)
-nested_map={"a": {"b": 2}}, path=("a", "b")
+def access_nested_map(nested_map, path):
+    """Access value in a nested map by path."""
+    for key in path:
+        nested_map = nested_map[key]
+    return nested_map
+
+
+def get_json(url):
+    """GET JSON data from a URL."""
+    return requests.get(url).json()
+
+
+def memoize(method):
+    """Memoization decorator."""
+    attr_name = "_{}".format(method.__name__)
+
+    @property
+    def wrapper(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, method(self))
+        return getattr(self, attr_name)
+
+    return wrapper
