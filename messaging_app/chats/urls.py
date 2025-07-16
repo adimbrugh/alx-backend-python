0007bc7c644d0030926edd4be6b2cc1d8c44cbf2
adimbrugh <haisamismail71@gmail.com>
2025-07-17 03:23:44 +0400

    Implement the endpoints to create a new conversation and send messages to an existing one.

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,8 @@
 
 
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter 
 from rest_framework_nested import routers
+from rest_framework.routers import DefaultRouter 
 from .views import ConversationViewSet, MessageViewSet, UserViewSet
 
 
@@ -17,24 +17,21 @@ router.register('messages', MessageViewSet)
 
 # This allows for nested routing under users
 # and conversations, enabling endpoints like /users/{user_id}/conversations/
-users_router = routers.NestedDefaultRouter(router, 'users', lookup='user') 
+users_router = routers.NestedDefaultRouter(router, r'users', lookup='user') 
 
 # Registering conversations under users
 # This allows for endpoints like /users/{user_id}/conversations/
-users_router.register('conversations', ConversationViewSet, basename='user-conversations')
+users_router.register(r'conversations', ConversationViewSet, basename='user-conversations')
 
 # Nested router for /conversations/{conversation_id}/messages/
 # This allows for nested routing under conversations
-conversation_router = routers.NestedDefaultRouter(users_router, 'conversations', lookup='conversation')
+conversation_router = routers.NestedDefaultRouter(users_router, r'conversations', lookup='conversation')
 
 # Registering messages under conversations
 # This allows for endpoints like /conversations/{conversation_id}/messages/
-conversation_router.register('messages', MessageViewSet, basename='conversation-messages')
+conversation_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 
-urlpatterns = router.urls + users_router.urls + conversation_router.urls
-
-"""
 # Main urlpatterns for the app
 urlpatterns = [
     
@@ -45,4 +42,3 @@ urlpatterns = [
 ]
 
 urlpatterns += router.urls # This line ensures that the main router's URLs are included in the urlpatterns
-"""
