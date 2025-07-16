ee63e55ea5d0dcdd422a7ebcaa9368b5613f827b
adimbrugh <haisamismail71@gmail.com>
2025-07-16 13:55:28 +0400

    create a new django project and install django rest framework.

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -153,3 +153,21 @@ DATABASES = {
         'PORT': env('DB_PORT'),
     }
 }
+
+
+rest_framework = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_yasg.generators.OpenAPISchemaGenerator',
+}
