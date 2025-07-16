473bcc51e8de8dfade8ed73b81d0eed1c0a45253
adimbrugh <haisamismail71@gmail.com>
2025-07-16 14:40:02 +0400

    implement API endpoints for conversations and messages.

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter  # ✅ explicitly named
 from .views import ConversationViewSet, MessageViewSet
 
 # ✅ Explicit router object
-router = DefaultRouter()
+router = [DefaultRouter()]
 router.register(r'conversations', ConversationViewSet, basename='conversations')
 router.register(r'messages', MessageViewSet, basename='messages')
 
