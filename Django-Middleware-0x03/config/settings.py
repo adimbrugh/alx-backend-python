4f05193397ccdceb41725361eb26e3048a5cf91c
adimbrugh <haisamismail71@gmail.com>
2025-07-24 14:04:41 +0400

    Create a middleware that logs each user’s requests to a file, including the timestamp, user and the request path.

--- Django-Middleware-0x03/config/settings.py
+++ Django-Middleware-0x03/config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', # Ensure this is at the top to allow CORS headers
+    'chats.middleware.RequestLoggingMiddleware', # ✅ Add this line (adjust path if needed)
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'chats.middleware.RequestLoggingMiddleware', # ✅ Add this line (adjust path if needed)
+    
 ]
 
 ROOT_URLCONF = 'config.urls'
