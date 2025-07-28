0b73a4a598238c4c7fbc3c628d4be6ac71639151
adimbrugh <haisamismail71@gmail.com>
2025-07-28 14:55:02 +0400

    Create a custom ORM for manager to filter unread messages for a user.

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -47,9 +47,8 @@ def fetch_thread(message):
 
 @login_required
 def unread_inbox_view(request):
-    unread_msgs = Message.unread.for_user(request.user)
-    
-    return unread_msgs
-    #return render(request, 'messaging/unread_inbox.html', {
-     #   'unread_messages': unread_msgs
-    #})
+    unread_msgs = Message.unread.unread.for_user(request.user)
+
+    return render(request, 'messaging/unread_inbox.html', {
+        'unread_messages': unread_msgs
+    })
