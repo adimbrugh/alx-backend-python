b48ae9d6cbb38336a6fccd603043c7f274ea7394
adimburgh <haisamismail71@gmail.com>
2025-08-20 13:34:31 +0400

    Set Up a GitHub Actions Workflow for Testing

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -147,16 +147,15 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # MySQL Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE'),
-        'USER': os.environ.get('MYSQL_USER'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
-        'HOST': 'db',
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME", "messaging_db"),
+        "USER": os.getenv("DB_USER", "root"),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "3306"),
     }
 }
-
 
 
 rest_framework = {
